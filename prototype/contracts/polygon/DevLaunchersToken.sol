d1be96b85fc783714e7812fc7e0ed1dedd3acb00
Maximilian Kling <an@maximilian-kling.de>
2021-06-03 21:09:01 +0200

    Adds basic Prototype for DevBots, which still has a lot of problems, but should be a good starting point

---
+++ prototype/contracts/polygon/DevLaunchersToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+
+contract DevLaunchersToken is ERC777, AccessControl {
+
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER");
+  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN");
+
+  constructor(address[] memory defaultOperators)
+    ERC777("DEVS", "DEVS", defaultOperators)
+    {
+      _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
+      _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+      _setupRole(ADMIN_ROLE, msg.sender);
+      _setupRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address receiver, uint256 amount, bytes calldata userData, bytes calldata operatorData) public onlyRole(MINTER_ROLE) {
+      _mint(receiver, amount, userData, operatorData);
+    }
+
+}
