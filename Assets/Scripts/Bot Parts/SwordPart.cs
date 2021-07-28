b52545cc052e4be7cce56c2b37d9df77fae6b265
Shaun <sdburke2006@yahoo.co.uk>
2021-07-28 11:15:02 +0100

    Made swordpart using animator for the sword rotation and rb.addthrust to make the bot lunge forward

--- Assets/Scripts/Bot Parts/SwordPart.cs
+++ Assets/Scripts/Bot Parts/SwordPart.cs
@@ -8,27 +8,63 @@ public class SwordPart : MonoBehaviour , IBotPart
     [SerializeField] private LayerMask enemy = default(LayerMask);
     [SerializeField] private Transform attackPos = default(Transform);
     [SerializeField] private float attackDistance = default(float);
-    [SerializeField] private float attackSize = default(float);
     [SerializeField] private float damage = default(float);
     [SerializeField] private float knockback = default(float);
-
+    [SerializeField] private float forwardThrustForce = default(float);
+    private bool isRunning;
+    private float timer;
+    private const float COOLDOWN = 2.0f;
+    private Animator swordAnimator;//Animator used for sword rotation
+    private Rigidbody2D botRigidbody;
     public void SetState(State state) {
         return;
     }
+    private void Start()
+    {
+        swordAnimator = GetComponent<Animator>();
+        botRigidbody = GetComponentInParent<Rigidbody2D>();
+        isRunning = true;
+    }
+    private void Update()
+    {
+        AttackStep();
+    }
+    public void AttackStep()
+    {
 
-    public void AttackStep(List<GameObject> activeBots) {
-    //Called each turn, checks for collision and calls its TakeDamage
+        if (isRunning)
+        {
 
-        int enemyDirection = GetComponent<BotSensor>().GetNearestSensedBotDirection();
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                timer = COOLDOWN; //Reset Timer
+                                  //get the enemy gameobject which is closest using the sensor script
+                                  //var enemy = sensor.GetNearestSensedBot().gameObject;
 
-        attackPos.localPosition = new Vector3(enemyDirection * attackDistance, 0, 0); //Faces attack at enemy, handled as local position to parent Bot
+                Collider2D collision = Physics2D.OverlapCircle(transform.position, attackDistance, enemy);
+                //OverlapBox for rectangular hitbox
 
-        Collider2D collision = Physics2D.OverlapCircle(attackPos.position, attackSize, enemy);
-        //OverlapBox for rectangular hitbox
-
-        if (collision != null) {
-            //collision.GetComponent<Health>().TakeDamage(damage);
-            //collision.GetComponent<Move>().TakeKnockback(enemyDirection, knockback);
+                if (collision != null)
+                {
+                    // Set trigger to play animation of sword rotating 
+                    swordAnimator.SetTrigger("swordAttack");
+                    // add thrust to lunge bot forwaard 
+                    botRigidbody.AddRelativeForce(new Vector2(forwardThrustForce, 0), ForceMode2D.Impulse);
+                    collision.GetComponent<Health>().TakeDamage(damage);
+                    //collision.GetComponent<Move>().TakeKnockback(enemyDirection, knockback);
+                }
+            }
         }
+    }
+    void OnDrawGizmosSelected()
+    {
+        // Display the attack radius when selected
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+
     }
 }
--- Assets/Scripts/Bot Parts/WheelPart.cs
+++ Assets/Scripts/Bot Parts/WheelPart.cs
@@ -19,16 +19,19 @@ public class WheelPart : MonoBehaviour , IBotPart
 
     public void Update() {
         MoveStep();
+
     }
 
     public void MoveStep() {
         if (isRunning) {
             int enemyDirection = sensor.GetNearestSensedBotDirection();
             float targetSpeed = enemyDirection * moveSpeed;
-            if (rb.velocity.x != (targetSpeed)) {
-                float increment = (targetSpeed - rb.velocity.x); //Mathf.Lerp(rb.velocity.x, targetSpeed, .5);
+            //changed if statement to check if velocity is lower than target speed and is now moving the bot using add force rather than explicitly changing the velocity
+            if (Mathf.Abs(rb.velocity.x) <= (targetSpeed)) {
+                //float increment = (targetSpeed - rb.velocity.x); //Mathf.Lerp(rb.velocity.x, targetSpeed, .5);
                 //This needs to be scaled to haappen over time instead of instantly
-                rb.velocity += new Vector2(increment, 0);
+                //rb.velocity += new Vector2(increment, 0);
+                rb.AddRelativeForce(new Vector2(targetSpeed, 0),ForceMode2D.Force);
             }
             //sensor.PlayAudio("Move");
         }
