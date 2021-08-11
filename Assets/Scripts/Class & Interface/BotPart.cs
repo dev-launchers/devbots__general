ca2cf3760d7cc36c4320ca08b5807d424518594d
LawrenceLinnell <76969895+LawrenceLinnell@users.noreply.github.com>
2021-08-11 19:40:09 -0400

    Add SerializeField coolDown to BotPart.cs
    Updated Botpart.cs to include a SerializeField for coolDown.

--- Assets/Scripts/Class & Interface/BotPart.cs
+++ Assets/Scripts/Class & Interface/BotPart.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public abstract class BotPart : MonoBehaviour
 {
-
+    [SerializeField] public float coolDown; 
     abstract public void SetState(State state);
     //[SerializeField] abstract private bool isRunning;
-
 }
