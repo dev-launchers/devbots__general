82e09073b0954861b4f76636ebbfb25b82ff9760
LawrenceLinnell <76969895+LawrenceLinnell@users.noreply.github.com>
2021-08-11 21:43:41 -0400

    Change BotPart coolDown from public to private
    Changed coolDown from public to private to improve security.

--- Assets/Scripts/Class & Interface/BotPart.cs
+++ Assets/Scripts/Class & Interface/BotPart.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public abstract class BotPart : MonoBehaviour
 {
-    [SerializeField] public float coolDown; 
+    [SerializeField] private float coolDown; 
     abstract public void SetState(State state);
     //[SerializeField] abstract private bool isRunning;
 }
