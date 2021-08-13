e759b8848456880c44b9b62120f354bd6fb5ed6d
LawrenceLinnell <76969895+LawrenceLinnell@users.noreply.github.com>
2021-08-13 19:13:13 -0400

    Update BotPart.cs
    Implement GetCoolDown to return bot coolDown time.

--- Assets/Scripts/Class & Interface/BotPart.cs
+++ Assets/Scripts/Class & Interface/BotPart.cs
@@ -5,4 +5,8 @@ public abstract class BotPart : MonoBehaviour
     [SerializeField] private float coolDown; 
     abstract public void SetState(State state);
     //[SerializeField] abstract private bool isRunning;
+
+    public float GetCoolDown(){
+        return coolDown;
+    }
 }
