ee8bce906dad0acffab53a96cee5f49c88c77c96
Xinyu <xinyudong1@gmail.com>
2021-10-05 10:50:24 -0400

    Fixed Start method on WingPart

--- Assets/Scripts/Bot Parts/WingsPart.cs
+++ Assets/Scripts/Bot Parts/WingsPart.cs
@@ -19,6 +19,7 @@ public class WingsPart : BotPart
         rb = gameObject.GetComponentInParent<Rigidbody2D>();
         sensor = GetComponentInParent<BotSensor>();
         controller = GetComponentInParent<BotController>();
+    }
 
     public void BackStep()
     {
