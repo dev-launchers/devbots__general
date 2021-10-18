190335ea4a1a822be1e92fcf8746b2428f7b2893
Sachin Kishan <sachinkishan02@gmail.com>
2021-10-18 08:49:27 +0530

    Update PartScriptableObject.cs

--- Assets/Scripts/Class & Interface/Audio/PartScriptableObject.cs
+++ Assets/Scripts/Class & Interface/Audio/PartScriptableObject.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName = "New Audio",menuName = "Bot Part")]
+[CreateAssetMenu(fileName = "New Audio",menuName = "Bot Part Sound")]
 public class PartScriptableObject : SoundScriptableObjectScript
 {
     [Range(0.1f,100f)]public float pitch;
