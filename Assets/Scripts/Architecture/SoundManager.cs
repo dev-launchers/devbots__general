1cd648244dd45f66da6d0d19c1bc754369eed1be
Sachin Kishan <sachinkishan02@gmail.com>
2021-10-18 08:24:13 +0530

    Bot part makes multiple sounds on call

--- Assets/Scripts/Architecture/SoundManager.cs
+++ Assets/Scripts/Architecture/SoundManager.cs
@@ -32,7 +32,7 @@ public class SoundManager : MonoBehaviour//this only manages one off sounds, UI 
 
     public void PlayMusic(string music)
     {
-        MusicScriptableObject s = Array.Find(musicList, sound => sound.name == music);
+        MusicScriptableObject s = Array.Find(musicList, sound => sound.musicName == music);
         musicSource.PlayOneShot(s.clip);
     }
 
--- Assets/Scripts/Class & Interface/Audio/BotPartAudio.cs
+++ Assets/Scripts/Class & Interface/Audio/BotPartAudio.cs
@@ -1,21 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+class BotPartSound
+{
+    public string nameOfSound;
+    public PartScriptableObject partSound;
+};
 
 public class BotPartAudio : MonoBehaviour
 {
     private BotAudioManager manager;
 
-    [SerializeField] private PartScriptableObject partSound;
+    [SerializeField] private BotPartSound[] botPartSounds;
     // Start is called before the first frame update
     void Start()
     {
         manager = GetComponentInParent<BotAudioManager>();
     }
 
-    public void PlaySound()
+    public void PlaySound(string soundName = " ")
     {
-        manager.PerformSound(partSound);
+        if (soundName == " ")
+        {
+            manager.PerformSound(botPartSounds[0].partSound);
+        }
+        else
+        {
+            BotPartSound s = Array.Find(botPartSounds, sound => sound.nameOfSound == soundName);
+            manager.PerformSound(s.partSound);
+        }
+        
     }
 
 }
