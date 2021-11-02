b629e53aebb9e208a1525c2f499deb131c138357
Sachin Kishan <sachinkishan02@gmail.com>
2021-11-02 20:46:51 +0530

    add basic asset menu extension

--- Assets/Scripts/Bot Functionality/Bot Actions/AttackAbility.cs
+++ Assets/Scripts/Bot Functionality/Bot Actions/AttackAbility.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "Attack Ability", menuName = "Ability Objects/Attack Ability", order = 1)]
 public class AttackAbility : BotAbility
 {
     public Vector2 dir;
