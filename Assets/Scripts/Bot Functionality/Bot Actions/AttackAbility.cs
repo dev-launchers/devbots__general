7bce29ff6e2f25b8dbeca5f3ec0877131c6783da
Sachin Kishan <sachinkishan02@gmail.com>
2021-11-05 12:57:54 +0530

    prepare test scene

--- Assets/Scripts/Bot Functionality/Bot Actions/AttackAbility.cs
+++ Assets/Scripts/Bot Functionality/Bot Actions/AttackAbility.cs
@@ -6,10 +6,23 @@ public class AttackAbility : BotAbility
 {
     public Vector2 dir;
     public float power;
-
-    public override void Activate(GameObject parent)
+    
+    
+    private Animator animator;//Animator used for sword rotation
+    private Rigidbody2D rb;
+    private BotSensor sensor;
+    private int enemyLayer;
+    public override void Activate(GameObject parent)//we may split this up further if necessary with more layers of abstraction
     {
         base.Activate(parent);
-        Debug.Log(name+"Perform attack ability");
+        
+        //set up
+        Animator = parent.GetComponent<Animator>();
+        rb = parent.GetComponentInParent<Rigidbody2D>();
+        sensor = parent.GetComponentInParent<BotSensor>();
+        enemyLayer = sensor.GetEnemyLayer();
+        
+        Debug.Log(parent.name+" performed attack ability");
+        
     }
 }
--- Assets/Scripts/Bot Functionality/BotAbilityHolder.cs
+++ Assets/Scripts/Bot Functionality/BotAbilityHolder.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
+
 public enum AbilityState
 {
     Ready,
@@ -18,9 +21,40 @@ public class BotAbilityHolder : MonoBehaviour
     private float coolDownTime;
 
     private float activeTime;
+
+    [SerializeField] private bool isRunning;
     // Update is called once per frame
     void Update()
     {
-        
+        switch(state)
+        {
+            case AbilityState.Ready:
+                if (isRunning/*condition for move*/)
+                {
+                    ability.Activate(gameObject);
+                    state = AbilityState.Active;
+                    activeTime = ability.activeTime;
+                }
+                break;
+            
+            case AbilityState.Active:
+                if (activeTime > 0) activeTime -= Time.deltaTime;
+                else
+                {
+                    state = AbilityState.Cooldown;
+                    coolDownTime = ability.coolDownTime;
+                    Debug.Log("Cooldown");
+                }
+                break;
+            
+            case AbilityState.Cooldown: 
+                if (coolDownTime > 0) coolDownTime -= Time.deltaTime;
+                else
+                {
+                    Debug.Log("Ready");
+                    state = AbilityState.Ready;
+                }
+                break;
+        }
     }
 }
