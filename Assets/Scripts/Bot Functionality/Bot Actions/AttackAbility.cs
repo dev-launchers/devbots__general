19199323797731b7cefd8e910252c2c7730aaa8b
Sachin Kishan <sachinkishan02@gmail.com>
2021-11-05 13:04:14 +0530

    make demo for sword slash using new system

--- Assets/Scripts/Bot Functionality/Bot Actions/AttackAbility.cs
+++ Assets/Scripts/Bot Functionality/Bot Actions/AttackAbility.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class AttackAbility : BotAbility
 {
     public Vector2 dir;
-    public float power;
-    
+    public float damage;
+    public float attackDistance;//what is this ?
+    public float knockback;
     
     private Animator animator;//Animator used for sword rotation
     private Rigidbody2D rb;
@@ -16,11 +17,33 @@ public class AttackAbility : BotAbility
     {
         base.Activate(parent);
         
-        //set up
-        Animator = parent.GetComponent<Animator>();
+        //SET UP
+        animator = parent.GetComponent<Animator>();
         rb = parent.GetComponentInParent<Rigidbody2D>();
         sensor = parent.GetComponentInParent<BotSensor>();
         enemyLayer = sensor.GetEnemyLayer();
+        ////////////
+        
+        //ACTION
+        // Set trigger to play animation of sword rotating 
+        animator.SetTrigger("swordAttack");
+        // add thrust to lunge bot forward 
+        Vector2 appliedForce = new Vector2(dir.x * sensor.GetNearestSensedBotDirection(), dir.y);
+        rb.AddRelativeForce(appliedForce, ForceMode2D.Impulse);
+
+        Vector2 attackPos = parent.transform.position + new Vector3(sensor.GetNearestSensedBotDirection(), 0, 0);
+        //Should be cleaned up, but currently creates Vector2 for current position + 1 in direction of enemy
+        Collider2D collision = Physics2D.OverlapCircle(attackPos, attackDistance); 
+        //Needs to attack only in front using swordPos
+
+        if (collision.gameObject.layer == enemyLayer)
+        {
+            Debug.Log("collision");
+            BotController collisionController = collision.transform.GetComponent<BotController>();
+            collisionController.TakeDamage(damage);
+            collisionController.ApplyForce(new Vector2(knockback * sensor.GetNearestSensedBotDirection(),0));
+        }
+        
         
         Debug.Log(parent.name+" performed attack ability");
         
